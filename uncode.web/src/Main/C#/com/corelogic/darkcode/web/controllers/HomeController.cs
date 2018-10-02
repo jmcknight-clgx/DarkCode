7df62f4ff6e7e6851057e02783271e2060311104
Thurston <cthurston@corelogic.com>
2018-10-02 15:12:38 -0500

    pants this time works

--- uncode.web/src/Main/C#/com/corelogic/darkcode/web/controllers/HomeController.cs
+++ uncode.web/src/Main/C#/com/corelogic/darkcode/web/controllers/HomeController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace uncode.web.src.Main.CSharp.com.corelogic.darkcode.web.controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BriansPantz<chaps<assless>>
     {
 
     }
