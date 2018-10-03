9fbfa2baf195b97f2ed47c7118a6c2d734bc341e
Thurston <cthurston@corelogic.com>
2018-10-03 13:50:11 -0500

    taking in the trash

--- uncode.web/src/Main/C#/com/corelogic/darkcode/web/controllers/HomeController.cs
+++ uncode.web/src/Main/C#/com/corelogic/darkcode/web/controllers/HomeController.cs
@@ -3,11 +3,20 @@ using uncode.web.src.Main.CSharp.com.corelogic.darkcode.pantz;
 using uncode.web.src.Main.CSharp.com.corelogic.darkcode.pantz.stuff7;
 using uncode.web.src.Main.CSharp.com.corelogic.darkcode.pantz.stuff7.wtf;
 
+
+using System.Threading.Tasks;
+
 namespace uncode.web.src.Main.CSharp.com.corelogic.darkcode.web.controllers
 {
     [AutoWired]
     public class HomeController : BriansPantz<chaps<Assless>>
     {
+
+        [Route("[controller]/Get")]
+        [HttpPost]
+        public async Task<HttpErrorCode> helloHell (HttpErrorCode uhOhSpaghettios, FormData form){
+            return new BrianzPants(form);
+        }
 
     }
 }
